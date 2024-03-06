d0f52011934c1c77a4c512b404199e6559915622
Barsch2006 <christian0511@gmx.de>
2024-03-06 20:43:33 +0100

    add footer

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,13 @@
 <template>
     <VApp>
         <VMain> </VMain>
+
+        <VFooter height="50px">
+            <a class="icon-link row" href="https://github.com/Barsch2006/iserv-sieve-script-generator">
+                <VIcon icon="mdi-github" />
+                <VBreadcrumbs :items="['v1.0.0', '&copy; Christian Fuchte']" />
+            </a>
+        </VFooter>
     </VApp>
 </template>
 
