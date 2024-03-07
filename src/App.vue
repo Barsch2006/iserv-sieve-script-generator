79d999ee24dfd4f666fd1266f1cecaf0c4f00f25
Barsch2006 <christian0511@gmx.de>
2024-03-07 18:10:49 +0100

    Add JavaScript enabled message and improve UI

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,56 @@
 <script lang="ts">
 export default {
-    data() {
+    data(): {
+        version: string;
+        generate: {
+            success: boolean;
+            error: string | undefined;
+            loading: boolean;
+            disabled: boolean;
+            code: string;
+        };
+        tab: string;
+        tabs: { value: string; icon: string; name: string }[];
+        mobile: boolean;
+    } {
         return {
             version: 'v1.0.0',
             generate: {
-                success: false,
+                success: true,
                 error: undefined as string | undefined,
                 loading: false,
                 disabled: true,
-                code: '',
+                code: 'require ["fileinto"]; \ntada',
             },
+            tab: 'start',
+            tabs: [
+                {
+                    value: 'start',
+                    icon: 'mdi-information-slab-circle-outline',
+                    name: 'Start',
+                },
+                {
+                    value: 'finish',
+                    icon: 'mdi-play-circle-outline',
+                    name: 'Generate',
+                },
+            ],
+            mobile: false,
+        };
+    },
+    methods: {
+        async copyGeneratedCode() {
+            try {
+                await window.navigator.clipboard.writeText(this.generate.code);
+            } catch {
+                this.generate.error = 'Could not copy code to clipboard';
+            }
+        },
+    },
+    beforeMount() {
+        this.mobile = window.innerWidth < 800;
+        window.onresize = () => {
+            this.mobile = window.innerWidth < 800;
         };
     },
 };
@@ -17,40 +58,76 @@ export default {
 
 <template>
     <VApp dark height="64px">
+        <VTabs v-model="tab" width="100%" :fixed-tabs="!mobile">
+            <VTab
+                :title="tab.name"
+                :prepend-icon="!mobile ? tab.icon : undefined"
+                v-for="tab in tabs"
+                :key="tab.value"
+                :value="tab.value"
+            >
+                <VIcon v-if="mobile" size="24px" :icon="tab.icon" />
+                <span v-else>{{ tab.name }}</span>
+            </VTab>
+        </VTabs>
         <VMain>
-            <VCard>
-                <VCardTitle class="text-center"> IServ Sieve Script Generator</VCardTitle>
-                <VCardSubtitle> Generate a Sieve script for IServ with ease. </VCardSubtitle>
-                <VAlert
-                    v-if="generate.error"
-                    style="margin-left: 10px; margin-right: 10px"
-                    title="Error"
-                    type="error"
-                    density="compact"
-                    width="calc(100% - 20px)"
-                    variant="flat"
-                    :msg="generate.error"
-                />
-                <VCardActions>
-                    <VBtn
-                        :color="
-                            generate.disabled ? '' : generate.success ? 'success' : generate.error ? 'error' : 'primary'
-                        "
-                        :loading="generate.loading"
-                        :disabled="generate.disabled"
-                        width="100%"
-                        variant="flat"
-                    >
-                        Generate
-                    </VBtn>
-                </VCardActions>
-                <VCardText v-if="generate.success">
-                    <VTextField v-model="generate.code" label="Generated Code" readonly />
-                </VCardText>
-            </VCard>
+            <VWindow v-model="tab">
+                <VWindowItem value="finish">
+                    <VCard>
+                        <VCardTitle class="text-center">Finish?</VCardTitle>
+                        <VCardSubtitle>Finished with your settings? Let's generate your code!</VCardSubtitle>
+                        <VAlert
+                            v-if="generate.error"
+                            style="margin-left: 10px; margin-right: 10px"
+                            title="Error"
+                            type="error"
+                            density="compact"
+                            width="calc(100% - 20px)"
+                            variant="flat"
+                            :msg="generate.error"
+                        />
+                        <VCardActions>
+                            <VBtn
+                                :color="
+                                    generate.disabled
+                                        ? ''
+                                        : generate.success
+                                          ? 'success'
+                                          : generate.error
+                                            ? 'error'
+                                            : 'primary'
+                                "
+                                :loading="generate.loading"
+                                :disabled="generate.disabled"
+                                width="100%"
+                                variant="flat"
+                            >
+                                Generate
+                            </VBtn>
+                        </VCardActions>
+                        <VCardText v-if="generate.success">
+                            <VTextarea
+                                v-model="generate.code"
+                                label="Generated Code"
+                                readonly
+                                bg-color="background"
+                                no-resize
+                                persistent-hint
+                                hint="Click the icon to copy the code to your clipboard"
+                                style="font-family: monospace; font-size: 10px;"
+                                :auto-grow="true"
+                            >
+                                <template v-slot:append-inner>
+                                    <VIcon @click="copyGeneratedCode" icon="mdi-share" />
+                                </template>
+                            </VTextarea>
+                        </VCardText>
+                    </VCard>
+                </VWindowItem>
+            </VWindow>
         </VMain>
 
-        <VFooter height="50px">
+        <VFooter height="48px">
             <a class="icon-link row" href="https://github.com/Barsch2006/iserv-sieve-script-generator">
                 <VIcon icon="mdi-github" />
                 <VBreadcrumbs id="version_info" :items="[version, '&copy; Christian Fuchte']" />
---
+++ src/components/Tab.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+export default {
+    props: {
+        mobile: Boolean,
+        value: String,
+        icon: String,
+        name: String,
+    },
+};
+</script>
+
+<template>
+    <VTab :value="value">
+        <VIcon v-if="mobile" size="24px" :icon="icon" />
+        <span v-else>{{ name }}</span>
+    </VTab>
+</template>
